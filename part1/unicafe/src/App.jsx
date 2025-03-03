6e459e1c8431679f931c30f59f23cbf905a7ff1f
Czar-sys <cesarbarbosa.989@gmail.com>
2025-03-03 08:25:29 -0500

    1.7 1.8 Completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,22 +4,42 @@ const Button = ({ text, onClick }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad, all }) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {(good - bad) / all}</p>
+      <p>Positive: {good / all * 100}%</p>
+    </>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGood = () => {
-    console.log(good);
-    setGood(good + 1);
+    const newGood = good + 1;
+    setGood(newGood);
+    setAll(newGood + neutral + bad);
   };
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1);
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    setAll(good + newNeutral + bad);
   };
 
   const handleBad = () => {
-    setBad(bad + 1);
+    const newBad = bad + 1;
+    setBad(newBad);
+    setAll(good + neutral + newBad);
   };
 
   return (
@@ -28,10 +48,7 @@ const App = () => {
       <Button text="good" onClick={handleGood} />
       <Button text="neutral" onClick={handleNeutral} />
       <Button text="bad" onClick={handleBad} />
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </>
   );
 };
